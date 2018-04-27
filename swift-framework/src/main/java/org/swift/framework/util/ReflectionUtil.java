c2482451b9d1380a78c5af25c52ea5dfb3f0012c
zhaww <zhaww@asto-inc.com>
2018-04-27 09:16:42 +0800

    优化日志

--- swift-framework/src/main/java/org/swift/framework/util/ReflectionUtil.java
+++ swift-framework/src/main/java/org/swift/framework/util/ReflectionUtil.java
@@ -1,5 +1,8 @@
 package org.swift.framework.util;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
 
@@ -8,6 +11,8 @@ import java.lang.reflect.Method;
  * create by ww
  **/
 public class ReflectionUtil {
+
+    private static final Logger logger = LoggerFactory.getLogger(ReflectionUtil.class);
 
     /**
      * 返回一个类实例
@@ -19,6 +24,7 @@ public class ReflectionUtil {
         try {
             instance = clazz.newInstance();
         } catch (Exception e) {
+            logger.error("反射创建实例失败");
             throw new RuntimeException(e);
         }
         return instance;
@@ -44,6 +50,7 @@ public class ReflectionUtil {
             method.setAccessible(true);
             result = method.invoke(object,args);
         } catch (Exception e) {
+            logger.error("反射执行方法失败");
             e.printStackTrace();
         }
         return result;
@@ -60,6 +67,7 @@ public class ReflectionUtil {
             field.setAccessible(true);
             field.set(object, arg);
         } catch (IllegalAccessException e) {
+            logger.error("反射设置成员变量失败");
             e.printStackTrace();
         }
     }
